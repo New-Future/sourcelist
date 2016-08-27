8646769f7e0d34d29ad94170bddc5318d7521153
New Future <new_future@msn.cn>
2016-08-28 02:52:58 +0800

    自动测速

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sourcelist
 自动设置软件源:notebook:
+
+```bash
+curl -#L https://newfuture.github.io/sourcelist/sh|sudo bash
+```
--- s.sh
+++ s.sh
@@ -1,6 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-SOURCE=https://ipv6.mirrors.ustc.edu.cn
+S1=https://ipv6.mirrors.ustc.edu.cn
+S2=https://mirrors6.tuna.tsinghua.edu.cn
+
+echo 'Test the speed...'
+V1=0
+V2=0
+for i in `seq 1 10`;do
+    V=$(curl -#o /dev/null -w'%{speed_download}' $S1)
+    V1=`echo $V1 + $V |bc`;
+    V=$(curl -#o /dev/null -w'%{speed_download}' $S2)
+    V2=`echo $V2 + $V |bc`;
+done 
+
+echo "finished test!"
+if (( $(echo "$V1 > $V2" |bc -l) )); then
+    echo "$V1 [$S1] >  $V2 [$S2]"
+    SOURCE=$V1;
+else
+    echo "$V1 [$S1] <  $V2 [$S2]"
+    SOURCE=$V2;
+fi;
 
 echo "change source list to $SOURCE"
 
